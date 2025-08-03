a94796439c459512b34be7f7cc71cf4c7c85706b
ghifaripg <ghifarip0g@gmail.com>
2025-08-03 19:51:18 +0700

    changing url api

--- main_api.py
+++ main_api.py
@@ -51,7 +51,7 @@ async def scan_email(file: UploadFile = File(...)):
         url_results = []
         for url in urls:
             try:
-                response = requests.post("http://54.179.177.191:8000/predict/url", json={"url": url}, timeout=10)
+                response = requests.post("http://13.213.46.180:8000/predict/url", json={"url": url}, timeout=10)
                 data = response.json()
                 url_results.append({
                     "url": url,
